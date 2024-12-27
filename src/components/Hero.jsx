e2f9cebdb9903f7b7e10d9eca288a76778c266d7
Venkatesh <venkateshkaviya1234@gmail.com>
2024-12-27 19:10:19 +0530

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/dhanyaravi30", "_blank")
+            window.open("https://github.com/", "_blank")
           }
           className='black_btn'
         >
